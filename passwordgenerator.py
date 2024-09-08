d1c4c64371f5b3ca926a2c37186166d8f7bc81f1
KALAGOTLARANGAREDDY <rangaredie@gmail.com>
2024-09-08 11:45:14 +0530

    Add files via upload

---
+++ passwordgenerator.py
@@ -0,0 +1,12 @@
+import random
+print("Welcome to Password generator")
+print("----------------------------")
+chars ='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*'
+numbers =int(input("Amount of passwords to generate: "))
+length =int(input("Enter length of password: "))
+print("Here are your Passwords")
+for password in range(numbers):
+    passwords=" "
+    for c in range(length):
+        passwords+=random.choice(chars)
+    print(passwords)
